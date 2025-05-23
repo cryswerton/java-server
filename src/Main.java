e9e398869c97a8d12077efe6854f21ddbfb48cea
cryswerton <cryswertons@gmail.com>
2025-05-23 23:06:28 +0100

    added basic routing

--- src/Main.java
+++ src/Main.java
@@ -22,13 +22,36 @@ public class Main {
                 ){
 
             String line = in.readLine();
+            if(line == null) return;
+
             System.out.println("Received: " + line);
 
-            String response =   "HTTP/1.1 200 OK\r\n"+
-                                "Content-Type: text/plain\r\n"+
-                                "Content-Length: 13\r\n"+
-                                "\r\n"+
-                                "Hello, World!";
+            String path = "/";
+            String[] parts = line.split(" ");
+            if(parts.length >= 2){
+                path = parts[1];
+            }
+
+            String body;
+            switch(path) {
+                case "/jane":
+                    body = "<html><body><h1>Hi, Jane.</h1></body></html>";
+                    break;
+                case "/john":
+                    body = "<html><body><h1>Hi, John.</h1></body></html>";
+                    break;
+                case "/dexter":
+                    body = "<html><body><h1>Hi, Dexter.</h1></body></html>";
+                    break;
+                default:
+                    body = "<html><body><h1>Welcome to my Java server</h1><p>Try <a href=\"/jane\">/jane</a> or <a href=\"/john\">/john</a> or <a href=\"/dexter\">/dexter</a></p></body></html>";
+            }
+
+            String response =   "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: text/html; charset=UTF-8\r\n" +
+                                "Content-Length: " + body.getBytes().length + "\r\n" +
+                                "\r\n" +
+                                body;
 
             out.write(response);
             out.flush();
